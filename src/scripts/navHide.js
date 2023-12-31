c538150d41d023c83e27a90a76f35c3e86d5e4de
bbgard <bgardiner@students.federation.edu.au>
2023-12-31 11:45:37 +1100

    Added privacy policy and cookie policy

--- src/scripts/navHide.js
+++ src/scripts/navHide.js
@@ -6,6 +6,9 @@ const nav = document.querySelector(".navbar"); // The navbar element
 const navHeight = nav.getBoundingClientRect().height; // The height of the navbar
 let prevScrollTop = 0; // The previous scroll position
 
+// Check if navbar contains "disable-transparency" class
+const disableEffects = nav.classList.contains("disable-transparency");
+
 window.addEventListener("scroll", () => {
   const currentScrollTop = window.scrollY || document.documentElement.scrollTop;
   if (currentScrollTop > prevScrollTop) {
@@ -17,11 +20,14 @@ window.addEventListener("scroll", () => {
     // Scrolling up
     nav.classList.remove("hide-nav");
 
-    // If scroll position is not at the top, make the navbar background opaque
-    if (window.scrollY > 10) {
-      nav.classList.add("opaque");
-    } else {
-      nav.classList.remove("opaque");
+    // Only add the opaque class if disableEffects is false
+    if (!disableEffects) {
+      // If scroll position is not at the top, make the navbar background opaque
+      if (window.scrollY > 10) {
+        nav.classList.add("opaque");
+      } else {
+        nav.classList.remove("opaque");
+      }
     }
   }
   prevScrollTop = currentScrollTop;
